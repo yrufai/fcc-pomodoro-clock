feab11ca6b1e57409f3de9de3ac7c663b414cd1d
yakubu rufai mahatma <yrufai23@gmail.com>
2019-02-05 23:21:03 +0000

    timer setup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Timer from "./timer";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowDown, faArrowUp } from "@fortawesome/free-solid-svg-icons";
 import "./App.scss";
@@ -8,19 +9,41 @@ class App extends Component {
     super(props);
     this.breakDecrement = this.breakDecrement.bind(this);
     this.breakIncrement = this.breakIncrement.bind(this);
+    this.sessionDecrement = this.sessionDecrement.bind(this);
+    this.sessionIncrement = this.sessionIncrement.bind(this);
     this.state = {
-      count: 15
+      breakCount: 0,
+      sessionCount: 0,
+      timer: new Date().toLocaleDateString()
     };
   }
 
   breakDecrement() {
     this.setState(prevState => {
-      return prevState--;
+      const count = prevState.breakCount--;
+      if (count <= 0) {
+        return (prevState.breakCount = 0);
+      }
+      return count;
     });
   }
   breakIncrement() {
     this.setState(prevState => {
-      return prevState++;
+      return prevState.breakCount++;
+    });
+  }
+  sessionDecrement() {
+    this.setState(state => {
+      const count = state.sessionCount--;
+      if (count <= 0) {
+        return (state.sessionCount = 0);
+      }
+      return count;
+    });
+  }
+  sessionIncrement() {
+    this.setState(state => {
+      return state.sessionCount++;
     });
   }
   render() {
@@ -29,28 +52,25 @@ class App extends Component {
         <div className="main-title"> Pomodoro Clock</div>
         <div className="length-control">
           <div id="break-label">Break Length </div>
-          <div id="break-decrement">
-            <FontAwesomeIcon
-              icon={faArrowDown}
-              size="sm"
-              onClick={this.breakDecrement}
-            />
+          <div id="break-decrement" onClick={this.breakDecrement}>
+            <FontAwesomeIcon icon={faArrowDown} size="sm" />
           </div>
-          <div id="break-length">{this.state.count}</div>
+          <div id="break-length">{this.state.breakCount}</div>
           <div id="break-increment" onClick={this.breakIncrement}>
             <FontAwesomeIcon icon={faArrowUp} size="sm" />
           </div>
         </div>
         <div className="length-control">
           <div id="session-label">Session Length </div>
-          <div id="session-decrement">
+          <div id="session-decrement" onClick={this.sessionDecrement}>
             <FontAwesomeIcon icon={faArrowDown} size="sm" />
           </div>
-          <div id="session-length">5</div>
-          <div id="session-increment">
+          <div id="session-length">{this.state.sessionCount} </div>
+          <div id="session-increment" onClick={this.sessionIncrement}>
             <FontAwesomeIcon icon={faArrowUp} size="sm" />
           </div>
         </div>
+        <Timer timer={this.state.timer} />
       </div>
     );
   }
---
+++ src/timer.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+const Timer = props => {
+  return <div>{props.timer}</div>;
+};
+export default Timer;
