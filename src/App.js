357a4afd336a6487d94c632e1c739c873e239a54
yakubu rufai mahatma <yrufai23@gmail.com>
2019-02-05 00:31:10 +0000

    added fontawesome

--- src/App.js
+++ src/App.js
@@ -1,16 +1,55 @@
 import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowDown, faArrowUp } from "@fortawesome/free-solid-svg-icons";
 import "./App.scss";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.breakDecrement = this.breakDecrement.bind(this);
+    this.breakIncrement = this.breakIncrement.bind(this);
+    this.state = {
+      count: 15
+    };
+  }
+
+  breakDecrement() {
+    this.setState(prevState => {
+      return prevState--;
+    });
+  }
+  breakIncrement() {
+    this.setState(prevState => {
+      return prevState++;
+    });
+  }
   render() {
     return (
       <div className="App">
         <div className="main-title"> Pomodoro Clock</div>
         <div className="length-control">
           <div id="break-label">Break Length </div>
-          <div id="break-decrement"> </div>
-          <div id="break-length">5</div>
-          <div id="break-increment"> </div>
+          <div id="break-decrement">
+            <FontAwesomeIcon
+              icon={faArrowDown}
+              size="sm"
+              onClick={this.breakDecrement}
+            />
+          </div>
+          <div id="break-length">{this.state.count}</div>
+          <div id="break-increment" onClick={this.breakIncrement}>
+            <FontAwesomeIcon icon={faArrowUp} size="sm" />
+          </div>
+        </div>
+        <div className="length-control">
+          <div id="session-label">Session Length </div>
+          <div id="session-decrement">
+            <FontAwesomeIcon icon={faArrowDown} size="sm" />
+          </div>
+          <div id="session-length">5</div>
+          <div id="session-increment">
+            <FontAwesomeIcon icon={faArrowUp} size="sm" />
+          </div>
         </div>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import "./index.scss";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faArrowDown, faArrowUp } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faArrowDown, faArrowUp);
+
 ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
